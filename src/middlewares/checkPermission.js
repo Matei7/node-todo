6eeba21a15bdfd6be540b0a183ad885a89184774
Matei <vmatei967@gmail.com>
2023-09-11 16:59:11 +0300

    permissions improve

--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -6,17 +6,26 @@ module.exports = permission => async ( req, res, next ) => {
 	const {authorization} = req.headers,
 		token = authorization?.split( ' ' )?.[ 1 ] || '';
 
+	if ( ! Array.isArray( permission ) ) {
+		permission = [ permission ];
+	}
+
 	if ( ! token ) {
 		logger.error( 'Token is required to access this resource' )
 		next( new Api400Error( 'Token is required to access this resource' ) )
 	} else {
-		const {role, userId} = await verifyJWT( token );
+		try {
+			const {role, userId} = await verifyJWT( token );
 
-		if ( role !== permission ) {
-			logger.error( `User ${userId} tried to access a resource without permission` )
-			next( new Api400Error( 'You do not have permission to access this resource' ) )
-		} else {
-			next()
+			if ( ! permission.includes( role ) ) {
+				logger.error( `User ${userId} tried to access a resource without permission` )
+				next( new Api400Error( 'You do not have permission to access this resource' ) )
+			} else {
+				next()
+			}
+		} catch ( e ) {
+			logger.error( `Expired token` )
+			next( new Api400Error( 'Expired token' ) )
 		}
 	}
 }
--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -5,8 +5,8 @@ const checkPermissions = require( "../middlewares/checkPermission" );
 const validator = require( '../middlewares/validator' );
 const {todoValidators} = require( '../validators' );
 
-router.get( '/todos', todoControllers.listAllTodos );
-router.get( '/todos/:id', todoControllers.getTodoById );
+router.get( '/todos', checkPermissions( ['WRITER', 'VIEWER'] ), todoControllers.listAllTodos );
+router.get( '/todos/:id', checkPermissions( ['WRITER', 'VIEWER'] ), todoControllers.getTodoById );
 router.post( '/todos', checkPermissions( 'WRITER' ), validator( todoValidators.create ), todoControllers.createTodo );
 router.put( '/todos/:id', checkPermissions( 'WRITER' ), validator( todoValidators.update ), todoControllers.updateTodoById );
 router.delete( '/todos/:id', checkPermissions( 'WRITER' ), todoControllers.deleteTodoById );
